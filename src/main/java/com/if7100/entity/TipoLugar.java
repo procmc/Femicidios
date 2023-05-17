9b3c2b8b1da567aa2b99701bad70eeaf015c2f4c
BarrantesPK <94096481+BarrantesPK@users.noreply.github.com>
2023-05-17 16:15:02 -0600

    Add files via upload

--- src/main/java/com/if7100/entity/TipoLugar.java
+++ src/main/java/com/if7100/entity/TipoLugar.java
@@ -1,3 +1,6 @@
+/**
+ * 
+ */
 package com.if7100.entity;
 
 /**
@@ -17,61 +20,61 @@ import jakarta.persistence.Table;
 
 public class TipoLugar {
 
-    /**
-     * Esta es la clase para el JPA entity de la tabla femicidios.TA_Usuarios
-     */
-    public TipoLugar() {
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * Esta es la clase para el JPA entity de la tabla femicidios.TA_Usuarios
+	 */
+	public TipoLugar() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer CI_Codigo;
+	
+	@Column (name = "CV_Titulo", nullable = false)
+	private String CVTitulo;
+	
+	@Column (name = "CV_Descripcion", nullable = false)
+	private String CVDescripcion;
+	
+	
+	public TipoLugar( String CVTitulo, String CVDescripcion) {
+		super();
+		this.CVTitulo = CVTitulo;
+		this.CVDescripcion = CVDescripcion;
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public Integer getCI_Codigo() {
+		return CI_Codigo;
+	}
 
+	public void setCI_Codigo(Integer cI_Codigo) {
+		CI_Codigo = cI_Codigo;
+	}
 
+	public String getCVTitulo() {
+		return CVTitulo;
+	}
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer CI_Codigo;
+	public void setCVTitulo(String cVTitulo) {
+		CVTitulo = cVTitulo;
+	}
 
-    @Column (name = "CV_Titulo", nullable = false)
-    private String CVTitulo;
+	public String getCVDescripcion() {
+		return CVDescripcion;
+	}
 
-    @Column (name = "CV_Descripcion", nullable = false)
-    private String CVDescripcion;
+	public void setCVDescripcion(String cVDescripcion) {
+		CVDescripcion = cVDescripcion;
+	}
 
-
-    public TipoLugar( String CVTitulo, String CVDescripcion) {
-        super();
-        this.CVTitulo = CVTitulo;
-        this.CVDescripcion = CVDescripcion;
-        // TODO Auto-generated constructor stub
-    }
-
-
-    public Integer getCI_Codigo() {
-        return CI_Codigo;
-    }
-
-    public void setCI_Codigo(Integer cI_Codigo) {
-        CI_Codigo = cI_Codigo;
-    }
-
-    public String getCVTitulo() {
-        return CVTitulo;
-    }
-
-    public void setCVTitulo(String cVTitulo) {
-        CVTitulo = cVTitulo;
-    }
-
-    public String getCVDescripcion() {
-        return CVDescripcion;
-    }
-
-    public void setCVDescripcion(String cVDescripcion) {
-        CVDescripcion = cVDescripcion;
-    }
-
-
-
-
-
+	
+	
+	
+	
 
 }
