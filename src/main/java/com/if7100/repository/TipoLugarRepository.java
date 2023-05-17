96c701e87d54f379b56c2735eecf3e91980d018f
BarrantesPK <94096481+BarrantesPK@users.noreply.github.com>
2023-05-17 16:15:46 -0600

    Add files via upload

--- src/main/java/com/if7100/repository/TipoLugarRepository.java
+++ src/main/java/com/if7100/repository/TipoLugarRepository.java
@@ -1,14 +1,13 @@
 package com.if7100.repository;
 
-import com.if7100.entity.TipoLugar;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.if7100.entity.TipoLugar; 
+
 @Repository
-public interface TipoLugarRepository extends JpaRepository<TipoLugar, Integer> {
+public interface TipoLugarRepository extends JpaRepository<TipoLugar, Integer>{
 
-    TipoLugar findByCVTitulo(String CVTitulo);
-
-    TipoLugar findByCVDescripcion(String CVDescripcion);
-
+	TipoLugar findByCVTitulo(String CVTitulo);
+	
 }
