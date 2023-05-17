0431d990ba7c6b260dc81248b359792508ed2cd8
BarrantesPK <94096481+BarrantesPK@users.noreply.github.com>
2023-05-17 16:16:25 -0600

    Add files via upload

--- src/main/java/com/if7100/service/TipoLugarService.java
+++ src/main/java/com/if7100/service/TipoLugarService.java
@@ -1,8 +1,10 @@
+/**
+ * 
+ */
 package com.if7100.service;
 
 import java.util.List;
 import com.if7100.entity.TipoLugar; //se debe importar para trabajar el usuario
-import com.if7100.entity.TipoRelacion;
 
 /**
  * @author kendall B
@@ -10,19 +12,17 @@ import com.if7100.entity.TipoRelacion;
  */
 public interface TipoLugarService {// se crean primero los servicios
 
-    List<TipoLugar> getAllTipoLugares();
+	List<TipoLugar> getAllTipoLugares();
 
-    TipoLugar saveTipoLugar(TipoLugar tipoLugar);
+	TipoLugar saveTipoLugar(TipoLugar tipoLugar);
+	
+	TipoLugar getTipoLugarByCodigo(Integer Codigo);
+	
+	TipoLugar updateTipoLugar(TipoLugar tipoLugar);
+	
+	void deleteTipoLugarByCodigo(Integer Codigo);
+	
+	TipoLugar getTipoLugarByCVTitulo(String CVTitulo);
 
-    TipoLugar getTipoLugarByCodigo(Integer Codigo);
-
-    TipoLugar updateTipoLugar(TipoLugar tipoLugar);
-
-    void deleteTipoLugarByCodigo(Integer Codigo);
-
-    TipoLugar getTipoLugarByTitulo(String CVTitulo);
-
-    TipoLugar getTipoLugarByDescripcion(String CVDescripcion);
-
-
+	
 }
