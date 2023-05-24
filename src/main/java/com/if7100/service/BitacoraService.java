119f779cefd03d09abe31530b138a042f5892a9b
TishFoster <tishary14@gmail.com>
2023-05-24 14:05:50 -0600

    Crear Service de bitacora

---
+++ src/main/java/com/if7100/service/BitacoraService.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.if7100.service;
+
+import java.util.List;
+
+
+import com.if7100.entity.Bitacora;
+/**
+ * @author tishary foster
+ * 
+ */
+public interface BitacoraService {
+	
+	List <Bitacora> getAllBitacoras();
+	Bitacora saveBitacora (Bitacora bitacora);
+	Bitacora getBitacoraById(Integer id);
+	Bitacora updateBitacora(Bitacora bitacora);
+	void deleteBitacoraById(Integer id);
+	Bitacora getBitacoraByCVUsuario(String CVUsuario);
+	
+
+}
