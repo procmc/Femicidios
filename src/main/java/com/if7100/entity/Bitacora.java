199f0573c671d49393ce6e1346ea4776625de76e
TishFoster <tishary14@gmail.com>
2023-05-24 14:06:34 -0600

    Crear entidad de bitacora

---
+++ src/main/java/com/if7100/entity/Bitacora.java
@@ -0,0 +1,107 @@
+/**
+ * 
+ */
+package com.if7100.entity;
+
+import java.security.Timestamp;
+
+
+/**
+ * @author tisha
+ *
+ */
+
+import jakarta.persistence.Column;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "th_bitacoras")
+
+/**
+ * Clase para el JPA Entity de la tabla femicidio.th_bitacora*/
+public class Bitacora {
+    
+	public Bitacora() {
+		
+	}
+	public Bitacora(String CV_DNI_Usuario, String CV_Descripcion) {
+		super();
+		this.CVUsuario = CV_DNI_Usuario;
+		this.CVDescripcion = CV_Descripcion;
+	}
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Integer CI_Id_Bitacora;
+	
+	@Column (name = "CI_Id", nullable = false)
+	private Integer CIId;
+	
+	@Column (name = "CV_DNI_Usuario", nullable = false)
+	private String CVUsuario;
+	
+	@Column (name = "CV_Descripcion", nullable = false)
+	private String CVDescripcion;
+
+
+	@Column (name = "CT_Fecha", nullable = false)
+	private Timestamp CTFecha;
+
+
+	public Integer getCI_Id_Bitacora() {
+		return CI_Id_Bitacora;
+	}
+
+
+	public void setCI_Id_Bitacora(Integer cI_Id_Bitacora) {
+		CI_Id_Bitacora = cI_Id_Bitacora;
+	}
+
+
+	public Integer getCIId() {
+		return CIId;
+	}
+
+
+	public void setCIId(Integer cIId) {
+		CIId = cIId;
+	}
+
+
+	public String getCVUsuario() {
+		return CVUsuario;
+	}
+
+
+	public void setCVUsuario(String cVUsuario) {
+		CVUsuario = cVUsuario;
+	}
+
+
+	public String getCVDescripcion() {
+		return CVDescripcion;
+	}
+
+
+	public void setCVDescripcion(String cVDescripcion) {
+		CVDescripcion = cVDescripcion;
+	}
+
+
+	public Timestamp getCTFecha() {
+		return CTFecha;
+	}
+
+
+	public void setCTFecha(Timestamp cTFecha) {
+		CTFecha = cTFecha;
+	}
+	
+
+}
