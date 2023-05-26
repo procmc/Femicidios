7d424afa2ba553fe98188ab819c8f7a92290ee50
TishFoster <tishary14@gmail.com>
2023-05-26 06:50:53 -0600

    Update ImputadoController.java

--- src/main/java/com/if7100/controller/ImputadoController.java
+++ src/main/java/com/if7100/controller/ImputadoController.java
@@ -1,5 +1,9 @@
 package com.if7100.controller;
 
+import com.if7100.entity.Bitacora; 
+import com.if7100.entity.Usuario;
+import com.if7100.service.BitacoraService;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -24,21 +28,30 @@ public class ImputadoController {
  private UsuarioRepository usuarioRepository;
  private Perfil perfil;
  private PerfilService perfilService;
+//instancias para control de bitacora
+private BitacoraService bitacoraService;
+private Usuario usuario;
+
  
- public ImputadoController (ImputadoService imputadoService, PerfilService perfilService, UsuarioRepository usuarioRepository) {
+ public ImputadoController (BitacoraService bitacoraService,
+ImputadoService imputadoService, PerfilService perfilService, UsuarioRepository usuarioRepository) {
 	 super();
 	 this.imputadoService=imputadoService;
 	 this.perfilService = perfilService;
      this.usuarioRepository = usuarioRepository;
+     this.bitacoraService= bitacoraService;
+
  }
  
  private void validarPerfil() {
  	
 		try {
-			
+			Usuario usuario=new Usuario();
+
 			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		    String username = authentication.getName();
-			
+		    this.usuario= new Usuario(usuarioRepository.findByCVCedula(username));
+
 			this.perfil = new Perfil(perfilService.getPerfilById(usuarioRepository.findByCVCedula(username).getCIPerfil()));
 			
 		}catch (Exception e) {
@@ -84,6 +97,8 @@ public class ImputadoController {
 	 try {
 			this.validarPerfil();
 			if(!this.perfil.getCVRol().equals("Consulta")) {
+				Bitacora bitacora=new Bitacora(this.usuario.getCI_Id(),this.usuario.getCVNombre(),this.perfil.getCVRol());
+				bitacoraService.saveBitacora(bitacora);
 				
 				imputadoService.deleteImputadoById(id);
 				 return "redirect:/imputados";
