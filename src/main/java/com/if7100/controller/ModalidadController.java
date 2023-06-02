00ee0dcebf3e72ed5bb58e12bfdacaf02f16b620
TishFoster <tishary14@gmail.com>
2023-06-02 12:08:07 -0600

    Update ModalidadController.java

--- src/main/java/com/if7100/controller/ModalidadController.java
+++ src/main/java/com/if7100/controller/ModalidadController.java
@@ -1,16 +1,5 @@
 package com.if7100.controller;
 
-import com.if7100.entity.Bitacora;
-
-import com.if7100.entity.Usuario;
-import com.if7100.entity.Modalidad;
-import com.if7100.entity.Perfil;
-
-import com.if7100.repository.UsuarioRepository;
-import com.if7100.service.BitacoraService;
-import com.if7100.service.ModalidadService;
-import com.if7100.service.PerfilService;
-
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -19,6 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.if7100.entity.Bitacora;
+import com.if7100.entity.Modalidad;
+import com.if7100.entity.Perfil;
+import com.if7100.entity.Usuario;
+import com.if7100.repository.UsuarioRepository;
+import com.if7100.service.BitacoraService;
+import com.if7100.service.ModalidadService;
+import com.if7100.service.PerfilService;
 
 @Controller
 public class ModalidadController {
@@ -39,20 +37,20 @@ public class ModalidadController {
         this.usuarioRepository = usuarioRepository;
         this.bitacoraService = bitacoraService;
     }
-    
+
 private void validarPerfil() {
-    	
+
 		try {
 			Usuario usuario=new Usuario();
 			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 			String username = authentication.getName();
 			this.usuario = new Usuario(usuarioRepository.findByCVCedula(username));
 			this.perfil = new Perfil(perfilService.getPerfilById(usuarioRepository.findByCVCedula(username).getCIPerfil()));
-			
+
 		}catch (Exception e) {
 			// TODO: handle exception
 		}
-		
+
 	}
 
     @GetMapping("/modalidades")
@@ -64,18 +62,18 @@ private void validarPerfil() {
     @GetMapping("/modalidades/new")
     public String createModalidadForm(Model model){
 
-    	
+
     	try {
 			this.validarPerfil();
 			if(!this.perfil.getCVRol().equals("Consulta")) {
-				
+
 				Modalidad modalidad= new Modalidad();
 		        model.addAttribute("modalidad", modalidad);
 		        return "modalidades/create_modalidad";
 			}else {
 				return "SinAcceso";
 			}
-			
+
 		}catch (Exception e) {
 			return "SinAcceso";
 		}
@@ -90,21 +88,21 @@ private void validarPerfil() {
 
     @GetMapping("/modalidades/{id}")
     public String deleteModalidad(@PathVariable Integer id){
-    	
+
     	try {
 			this.validarPerfil();
 			if(!this.perfil.getCVRol().equals("Consulta")) {
-				
+
 				//INSERTAR EN BITACORA
 				Bitacora bitacora = new Bitacora(this.usuario.getCI_Id(), this.usuario.getCVNombre(), this.perfil.getCVRol());
-				
+
 				bitacoraService.saveBitacora(bitacora);
 				modalidadService.deleteModalidadById(id);
 		        return "redirect:/modalidades";
 			}else {
 				return "SinAcceso";
 			}
-			
+
 		}catch (Exception e) {
 			return "SinAcceso";
 		}
@@ -113,22 +111,22 @@ private void validarPerfil() {
     @GetMapping("/modalidades/edit/{id}")
     public String editModalidadForm(@PathVariable Integer id, Model model){
 
-    	
+
     	try {
 			this.validarPerfil();
 			if(!this.perfil.getCVRol().equals("Consulta")) {
-				
+
 				model.addAttribute("modalidad", modalidadService.getModalidadById(id));
 		        return "modalidades/edit_modalidad";
 			}else {
 				return "SinAcceso";
 			}
-			
+
 		}catch (Exception e) {
 			return "SinAcceso";
 		}
     }
-    
+
 
     @PostMapping("/modalidades/{id}")
     public String updateModalidad(@PathVariable Integer id, @ModelAttribute("modalidad") Modalidad modalidad){
