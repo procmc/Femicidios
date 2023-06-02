c36b7a05e8a190242e68cdfc5d542111c0422c27
TishFoster <tishary14@gmail.com>
2023-06-02 12:09:06 -0600

    Update UsuarioController.java

--- src/main/java/com/if7100/controller/UsuarioController.java
+++ src/main/java/com/if7100/controller/UsuarioController.java
@@ -1,11 +1,7 @@
 /**
- * 
+ *
  */
 package com.if7100.controller;
-
-import com.if7100.entity.Bitacora; 
-import com.if7100.entity.Usuario;
-import com.if7100.service.BitacoraService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -18,23 +14,21 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.if7100.entity.Hecho;
+import com.if7100.entity.Bitacora;
 import com.if7100.entity.Perfil;
-/**
- * @author Liss
- * Fecha: 11 de abril del 2023
- */
 import com.if7100.entity.Usuario;
 import com.if7100.repository.UsuarioRepository;
+import com.if7100.service.BitacoraService;
 import com.if7100.service.PerfilService;
 import com.if7100.service.UsuarioService;
+
 import jakarta.validation.Valid;
-   
+
 
 @Controller
 public class UsuarioController {
 	@Autowired
-	
+
 	private UsuarioService usuarioService;
 	//instancias para control de acceso
     private UsuarioRepository usuarioRepository;
@@ -47,7 +41,7 @@ public class UsuarioController {
 
 //prueba
 //private SessionRegistry sessionRegistry;
- 
+
  public UsuarioController(BitacoraService bitacoraService,
 UsuarioService usuarioService, PerfilService perfilService, UsuarioRepository usuarioRepository) {
 	 super();
@@ -57,9 +51,9 @@ UsuarioService usuarioService, PerfilService perfilService, UsuarioRepository us
      this.bitacoraService= bitacoraService;
 
  }
- 
+
  private void validarPerfil() {
- 	
+
 		try {
 			Usuario usuario=new Usuario();
 
@@ -68,55 +62,55 @@ UsuarioService usuarioService, PerfilService perfilService, UsuarioRepository us
 		    this.usuario= new Usuario(usuarioRepository.findByCVCedula(username));
 
 			this.perfil = new Perfil(perfilService.getPerfilById(usuarioRepository.findByCVCedula(username).getCIPerfil()));
-			
+
 		}catch (Exception e) {
 			// TODO: handle exception
 		}
-		
+
 	}
- 
+
  @GetMapping("/usuarios")
  public String listStudents(Model model) {
-	 
+
 	 try {
 			this.validarPerfil();
 			if(this.perfil.getCVRol().equals("Administrador")) {
-				
+
 				 model.addAttribute("usuarios",usuarioService.getAllUsuarios());
 				 return "usuarios/usuarios";
 			}else {
 				return "SinAcceso";
 			}
-			
+
 		}catch (Exception e) {
 			return "SinAcceso";
 		}
  }
- 
- 
+
+
  // creacion de un nuevo usuario
  @GetMapping("/usuarios/new")
  public String createUsuarioForm(Model model) {
-	 
+
 	 try {
 			this.validarPerfil();
 			if(this.perfil.getCVRol().equals("Administrador")) {
-				
+
 				Usuario usuario=new Usuario();
 				 model.addAttribute("usuario", usuario);
 				 return "usuarios/create_usuario";
 			}else {
 				return "SinAcceso";
 			}
-			
+
 		}catch (Exception e) {
 			return "SinAcceso";
 		}
  }
- 
 
- 
- 
+
+
+
  @PostMapping("/usuarios")
  public String saveUsuario(@Valid @ModelAttribute("usuario") Usuario usuario, BindingResult result) {
 	 if(result.hasErrors()) {
@@ -126,57 +120,57 @@ UsuarioService usuarioService, PerfilService perfilService, UsuarioRepository us
 		 return "redirect:/usuarios";
 	 }
  }
- 
- 
- 
- 
- 
- 
+
+
+
+
+
+
  //Eliminar Usuario
  @GetMapping("/usuarios/{Id}")
  public String deleteUsuario(@PathVariable Integer Id) {
-	 
+
 	 try {
 			this.validarPerfil();
 			if(this.perfil.getCVRol().equals("Administrador")) {
 				Bitacora bitacora=new Bitacora(this.usuario.getCI_Id(),this.usuario.getCVNombre(),this.perfil.getCVRol());
 				bitacoraService.saveBitacora(bitacora);
-				
+
 				usuarioService.deleteUsuarioById(Id);
 				return "redirect:/usuarios?Exito";
 			}else {
 				return "SinAcceso";
 			}
-			
+
 		}catch (Exception e) {
 			return "SinAcceso";
 		}
  }
- 
- 
- 
- 
- 
- 
+
+
+
+
+
+
  //EditarUsuario
  @GetMapping("/usuarios/edit/{Id}")
  public String editUsuarioForm(@PathVariable Integer Id, Model model) {
-	 
+
 	 try {
 			this.validarPerfil();
 			if(this.perfil.getCVRol().equals("Administrador")) {
-				
+
 				model.addAttribute("usuario", usuarioService.getUsuarioById(Id));
 				return "usuarios/edit_usuario";
 			}else {
 				return "SinAcceso";
 			}
-			
+
 		}catch (Exception e) {
 			return "SinAcceso";
 		}
  }
- 
+
  @PostMapping("/usuarios/{Id}")
  public String updateUsuario(@PathVariable Integer Id, @ModelAttribute("usuario") Usuario usuario) {
 	 Usuario existingUsuario=usuarioService.getUsuarioById(Id);
@@ -194,10 +188,10 @@ UsuarioService usuarioService, PerfilService perfilService, UsuarioRepository us
  public  ResponseEntity<?> getDetailsSession(){
 	 String sessionId="";
 	 User userObject=null;
-	 
-	 
+
+
 	 List<Object> sessions= sessionRegistry.getAllPrincipals();
-	 
+
 	 for(Object session : sessions) {
 		 if(session instanceof User) {
 			 userObject=(User) session;
@@ -207,12 +201,12 @@ UsuarioService usuarioService, PerfilService perfilService, UsuarioRepository us
 			 sessionId=sessionInformation.getSessionId();
 		 }
 	 }
-	 
+
 	 Map<String, Object> response= new HashMap<>();
 	 response.put("response","Hello");
 	 response.put("sessionId",sessionId);
 	 response.put("sessionUser",userObject);
 	 return ResponseEntity.ok(response);
  }*/
- 
+
 }
