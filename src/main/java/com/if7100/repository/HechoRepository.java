9c311caccf53970a808488bb710dba4ba8648b2d
TishFoster <tishary14@gmail.com>
2023-06-02 12:10:59 -0600

    Update HechoRepository.java

--- src/main/java/com/if7100/repository/HechoRepository.java
+++ src/main/java/com/if7100/repository/HechoRepository.java
@@ -1,8 +1,9 @@
 package com.if7100.repository;
 
-import com.if7100.entity.Hecho;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.if7100.entity.Hecho;
 
 @Repository
 public interface HechoRepository extends JpaRepository<Hecho, Integer> {
