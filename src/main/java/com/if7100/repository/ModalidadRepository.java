1b993cf201f4cfdb5aa35e385c638e4419384448
TishFoster <tishary14@gmail.com>
2023-06-02 12:11:11 -0600

    Update ModalidadRepository.java

--- src/main/java/com/if7100/repository/ModalidadRepository.java
+++ src/main/java/com/if7100/repository/ModalidadRepository.java
@@ -1,8 +1,9 @@
 package com.if7100.repository;
 
-import com.if7100.entity.Modalidad;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.if7100.entity.Modalidad;
 
 @Repository
 public interface ModalidadRepository extends JpaRepository<Modalidad, Integer> {
