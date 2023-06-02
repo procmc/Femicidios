0fbd2353ab0728f60b559eba3869fc3b8224ef66
TishFoster <tishary14@gmail.com>
2023-06-02 12:09:36 -0600

    Update HechoOrganismo.java

--- src/main/java/com/if7100/entity/HechoOrganismo.java
+++ src/main/java/com/if7100/entity/HechoOrganismo.java
@@ -1,6 +1,11 @@
 package com.if7100.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "hechos_organismos")
