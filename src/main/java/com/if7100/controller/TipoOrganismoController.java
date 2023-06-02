6e29d818b4f5b5a188f9eb730f57e7ab2b592c6d
TishFoster <tishary14@gmail.com>
2023-06-02 12:08:56 -0600

    Update TipoOrganismoController.java

--- src/main/java/com/if7100/controller/TipoOrganismoController.java
+++ src/main/java/com/if7100/controller/TipoOrganismoController.java
@@ -1,11 +1,7 @@
 /**
- * 
+ *
  */
 package com.if7100.controller;
-
-import com.if7100.entity.Bitacora; 
-import com.if7100.entity.Usuario;
-import com.if7100.service.BitacoraService;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,15 +12,17 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.if7100.entity.Bitacora;
+import com.if7100.entity.Perfil;
+import com.if7100.entity.TipoOrganismo;
+import com.if7100.entity.Usuario;
+import com.if7100.repository.UsuarioRepository;
+import com.if7100.service.BitacoraService;
 import com.if7100.service.PerfilService;
 /**
  * @author Adam Smasher
  */
 import com.if7100.service.TipoOrganismoService;
-import com.if7100.entity.Hecho;
-import com.if7100.entity.Perfil;
-import com.if7100.entity.TipoOrganismo;
-import com.if7100.repository.UsuarioRepository;
 
 @Controller
 public class TipoOrganismoController {
@@ -46,9 +44,9 @@ TipoOrganismoService tipoOrganismoService, PerfilService perfilService, UsuarioR
     this.bitacoraService= bitacoraService;
 
 }
-	
+
 	private void validarPerfil() {
-	
+
 		try {
 			Usuario usuario=new Usuario();
 
@@ -57,11 +55,11 @@ TipoOrganismoService tipoOrganismoService, PerfilService perfilService, UsuarioR
 		    this.usuario= new Usuario(usuarioRepository.findByCVCedula(username));
 
 			this.perfil = new Perfil(perfilService.getPerfilById(usuarioRepository.findByCVCedula(username).getCIPerfil()));
-			
+
 		}catch (Exception e) {
 			// TODO: handle exception
 		}
-		
+
 	}
 
 	@GetMapping("/tipoOrganismo")
@@ -72,77 +70,77 @@ TipoOrganismoService tipoOrganismoService, PerfilService perfilService, UsuarioR
 
 	@GetMapping("/tipoOrganismo/new")
 	public String CreateTipoOrganismoForm (Model model) {
-		
+
 		try {
 			this.validarPerfil();
 			if(!this.perfil.getCVRol().equals("Consulta")) {
-				
+
 				TipoOrganismo tipoOrganismo= new TipoOrganismo();
 				model.addAttribute("tipoOrganismo", tipoOrganismo);
 				return "tipoOrganismo/create_tipoOrganismo";
 			}else {
 				return "SinAcceso";
 			}
-			
+
 		}catch (Exception e) {
 			return "SinAcceso";
 		}
 	}
-	
+
 	@PostMapping("/tipoOrganismo")
 	public String saveTipoOrganismo (@ModelAttribute("tipoOrganismo") TipoOrganismo tipoOrganismo) {
 	    tipoOrganismoService.saveTipoOrganismo(tipoOrganismo);
-		return "redirect:/tipoOrganismo";	
+		return "redirect:/tipoOrganismo";
 	}
-	
+
 
 	@GetMapping("/tipoOrganismo/{Codigo}")
 	public String deleteTipoOrganismoByCodigo (@PathVariable Integer Codigo) {
-		
+
 		try {
 			this.validarPerfil();
 			if(!this.perfil.getCVRol().equals("Consulta")) {
 				Bitacora bitacora=new Bitacora(this.usuario.getCI_Id(),this.usuario.getCVNombre(),this.perfil.getCVRol());
 				bitacoraService.saveBitacora(bitacora);
-				
+
 				tipoOrganismoService.deleteTipoOrganismoByCodigo(Codigo);
-				return "redirect:/tipoOrganismo";	
+				return "redirect:/tipoOrganismo";
 			}else {
 				return "SinAcceso";
 			}
-			
+
 		}catch (Exception e) {
 			return "SinAcceso";
 		}
 	}
-	
+
 
 	@GetMapping("/tipoOrganismo/edit/{Codigo}")
 	public String editTipoOrganismoForm (@PathVariable Integer Codigo,Model model) {
-		
+
 		try {
 			this.validarPerfil();
 			if(!this.perfil.getCVRol().equals("Consulta")) {
-				
+
 				model.addAttribute("tipoOrganismo", tipoOrganismoService.getTipoOrganismoByCodigo(Codigo));
-				return "tipoOrganismo/edit_tipoOrganismo";	
+				return "tipoOrganismo/edit_tipoOrganismo";
 			}else {
 				return "SinAcceso";
 			}
-			
+
 		}catch (Exception e) {
 			return "SinAcceso";
-		}	
+		}
 	}
-	
+
 	@PostMapping("/tipoOrganismo/{Codigo}")
 	public String updateOrganismoLugar (@PathVariable Integer Codigo ,@ModelAttribute("tipoOrganismo") TipoOrganismo tipoOrganismo, Model model) {
-	   
+
 		TipoOrganismo existingTipoOrganismo = tipoOrganismoService.getTipoOrganismoByCodigo(Codigo);
 	    existingTipoOrganismo.setCI_Codigo(Codigo);
 	    existingTipoOrganismo.setCVTitulo(tipoOrganismo.getCVTitulo());
 	    existingTipoOrganismo.setCVDescripcion(tipoOrganismo.getCVDescripcion());
 		tipoOrganismoService.updateTipoOrganismo(existingTipoOrganismo);
-		return "redirect:/tipoOrganismo";	
+		return "redirect:/tipoOrganismo";
 	}
 }
