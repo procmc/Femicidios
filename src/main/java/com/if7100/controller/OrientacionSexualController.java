b4a940bbac8bc26725378065a39b5b7a3248f7c7
TishFoster <tishary14@gmail.com>
2023-06-02 12:08:24 -0600

    Update OrientacionSexualController.java

--- src/main/java/com/if7100/controller/OrientacionSexualController.java
+++ src/main/java/com/if7100/controller/OrientacionSexualController.java
@@ -1,16 +1,5 @@
 package com.if7100.controller;
 
-import com.if7100.entity.Bitacora; 
-import com.if7100.entity.Usuario;
-import com.if7100.service.BitacoraService;
-
-import com.if7100.entity.Hecho;
-import com.if7100.entity.OrientacionSexual;
-import com.if7100.entity.Perfil;
-import com.if7100.repository.UsuarioRepository;
-import com.if7100.service.OrientacionSexualService;
-import com.if7100.service.PerfilService;
-
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -20,12 +9,18 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.if7100.entity.Bitacora;
 import com.if7100.entity.OrientacionSexual;
+import com.if7100.entity.Perfil;
+import com.if7100.entity.Usuario;
+import com.if7100.repository.UsuarioRepository;
+import com.if7100.service.BitacoraService;
 import com.if7100.service.OrientacionSexualService;
+import com.if7100.service.PerfilService;
 
 @Controller
 public class OrientacionSexualController {
-	
+
  private OrientacionSexualService orientacionService;
 //instancias para control de acceso
  private UsuarioRepository usuarioRepository;
@@ -35,7 +30,7 @@ public class OrientacionSexualController {
 private BitacoraService bitacoraService;
 private Usuario usuario;
 
- 
+
  public OrientacionSexualController(BitacoraService bitacoraService,
 OrientacionSexualService orientacionService, PerfilService perfilService, UsuarioRepository usuarioRepository) {
 	 super();
@@ -45,45 +40,45 @@ OrientacionSexualService orientacionService, PerfilService perfilService, Usuari
      this.bitacoraService= bitacoraService;
 
  }
- 
+
 
  private void validarPerfil() {
- 	
+
 		try {
 			Usuario usuario=new Usuario();
 			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		    String username = authentication.getName();
-		    
+
 		    this.usuario= new Usuario(usuarioRepository.findByCVCedula(username));
 
 			this.perfil = new Perfil(perfilService.getPerfilById(usuarioRepository.findByCVCedula(username).getCIPerfil()));
-			
+
 		}catch (Exception e) {
 			// TODO: handle exception
 		}
-		
+
 	}
- 
+
  @GetMapping("/orientacionessexuales")
  public String listOrientacionesSexuales(Model model) {
-	 
+
 	 model.addAttribute("orientacionesSexuales",orientacionService.getAllOrientacionesSexuales());
 	 return "orientacionesSexuales/orientacionesSexuales";
  }
- 
+
  @GetMapping("/orientacionessexuales/new")
  public String createOrientacionSexualForm(Model model) {
-	 
+
 	 try {
 			this.validarPerfil();
 			if(!this.perfil.getCVRol().equals("Consulta")) {
-				
+
 				model.addAttribute("orientacionSexual",new OrientacionSexual());
 				return "orientacionesSexuales/create_orientacionSexual";
 			}else {
 				return "SinAcceso";
 			}
-			
+
 		}catch (Exception e) {
 			return "SinAcceso";
 		}
@@ -91,35 +86,35 @@ OrientacionSexualService orientacionService, PerfilService perfilService, Usuari
 
  @GetMapping("/orientacionesSexuales")
  public String listStudents(Model model) {
-	 
+
 	 model.addAttribute("orientacionesSexuales",orientacionService.getAllOrientacionesSexuales());
 	 return "orientacionessexuales/orientacionesSexuales";
  }
- 
+
  @GetMapping("/orientacionesSexuales/new")
  public String createUsuarioForm(Model model) {
-	 
+
 	 try {
 			this.validarPerfil();
 			if(!this.perfil.getCVRol().equals("Consulta")) {
-				
+
 				model.addAttribute("orientacion",new OrientacionSexual());
 				return "orientacionessexuales/create_orientacionesSexuales";
 			}else {
 				return "SinAcceso";
 			}
-			
+
 		}catch (Exception e) {
 			return "SinAcceso";
 		}
  }
- 
+
  @PostMapping("/orientacionesSexuales")
  public String saveOrientacion(@ModelAttribute("orientacion") OrientacionSexual orientacion) {
 	 orientacionService.saveOrientacionSexual(orientacion);
 	 return "redirect:/orientacionesSexuales";
  }
- 
+
  @GetMapping("/orientacionesSexuales/{id}")
  public String deleteOrientacion(@PathVariable int id) {
 
@@ -128,47 +123,47 @@ OrientacionSexualService orientacionService, PerfilService perfilService, Usuari
 			if(!this.perfil.getCVRol().equals("Consulta")) {
 				Bitacora bitacora=new Bitacora(this.usuario.getCI_Id(),this.usuario.getCVNombre(),this.perfil.getCVRol());
 				bitacoraService.saveBitacora(bitacora);
-				
+
 				orientacionService.deleteOrientacionSexualByCodigo(id);
 				 return "redirect:/orientacionesSexuales";
 			}else {
 				return "SinAcceso";
 			}
-			
+
 		}catch (Exception e) {
 			return "SinAcceso";
 		}
  }
- 
+
  @GetMapping("/orientacionesSexuales/edit/{id}")
  public String editOrientacionForm(Model model,@PathVariable int id) {
 	 try {
 			this.validarPerfil();
 			if(!this.perfil.getCVRol().equals("Consulta")) {
-				
+
 				model.addAttribute("orientacion", orientacionService.getOrientacionSexualByCodigo(id));
 				 return "orientacionessexuales/edit_orientacionesSexuales";
 			}else {
 				return "SinAcceso";
 			}
-			
+
 		}catch (Exception e) {
 			return "SinAcceso";
 		}
  }
- 
+
  @PostMapping("/orientacionesSexuales/{id}")
  public String updateOrientacionSexual(@PathVariable int id, @ModelAttribute("orientacion") OrientacionSexual orientacion, Model model) {
 	 OrientacionSexual existingOrientacion=orientacionService.getOrientacionSexualByCodigo(id);
 	 existingOrientacion.setCI_Codigo(id);
 	 existingOrientacion.setCVTitulo(orientacion.getCVTitulo());
 	 existingOrientacion.setCVDescripcion(orientacion.getCVDescripcion());
-	 
+
 	 orientacionService.updateOrientacionSexual(existingOrientacion);
 	 return "redirect:/orientacionesSexuales";
  }
- 
- 
- 
- 
+
+
+
+
 }
