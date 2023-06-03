892d15bf2d8873343a0d3e13d611a5ddc8b9a554
TishFoster <tishary14@gmail.com>
2023-06-02 18:07:46 -0600

    update

--- src/main/java/com/if7100/entity/Bitacora.java
+++ src/main/java/com/if7100/entity/Bitacora.java
@@ -1,20 +1,15 @@
 /**
- * 
+ *
  */
 package com.if7100.entity;
 
-
-import java.sql.Timestamp;
 import java.util.Date;
-
 /**
  * @author tisha
  *
  */
 
 import jakarta.persistence.Column;
-
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -27,33 +22,33 @@ import jakarta.persistence.Table;
 /**
  * Clase para el JPA Entity de la tabla femicidio.th_bitacora*/
 public class Bitacora {
-    
+
 	public Bitacora() {
-		
+
 	}
-	public Bitacora(Integer CIId, String CV_DNI_Usuario, String CV_Descripcion, String CVRol) {
+	public Bitacora(Integer CIId, String CV_DNI_Usuario, String CV_Rol, String CV_Descripcion) {
 		super();
 		this.CVUsuario = CV_DNI_Usuario;
+		this.CVRol= CV_Rol; 
 		this.CVDescripcion = CV_Descripcion;
 		this.CIId = CIId;
 		this.CTFecha = new Date();
-		this.CVRol = CVRol;
 	}
 
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Integer CI_Id_Bitacora;
-	
+
 	@Column (name = "CI_Id", nullable = false)
 	private Integer CIId;
-	
-	
+
+
 	@Column (name = "CV_DNI_Usuario", nullable = false)
 	private String CVUsuario;
-	
+
 	@Column (name = "CV_Rol", nullable = false)
 	private String CVRol;
-	
+
 	@Column (name = "CV_Descripcion", nullable = false)
 	private String CVDescripcion;
 
@@ -62,25 +57,12 @@ public class Bitacora {
 	private Date CTFecha;
 
 
-	public Integer getCI_Id_Bitacora() {
+	public Integer getCIIdBitacora() {
 		return CI_Id_Bitacora;
 	}
 
-	
-	/*public Connection getConexionBD() {
-		return conexionBD;
-	}
-	public void setConexionBD(Connection conexionBD) {
-		this.conexionBD = conexionBD;
-	}
-	public void Conexion() throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		java.sql.Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/femicidios","tishary","1234");
-		conexionBD= conexion;
-	}*/
-
-	public void setCI_Id_Bitacora(Integer cI_Id_Bitacora) {
-		CI_Id_Bitacora = cI_Id_Bitacora;
+	public void setCIIdBitacora(Integer cIIdBitacora) {
+		CI_Id_Bitacora = cIIdBitacora;
 	}
 
 
@@ -93,7 +75,7 @@ public class Bitacora {
 		CIId = cIId;
 	}
 
-	
+
 	public String getCVUsuario() {
 		return CVUsuario;
 	}
@@ -104,6 +86,13 @@ public class Bitacora {
 	}
 
 
+	public String getCVRol() {
+		return CVRol;
+	}
+
+	public void setCVRol(String cVRol) {
+		CVRol = cVRol;
+	}
 	public String getCVDescripcion() {
 		return CVDescripcion;
 	}
@@ -118,18 +107,10 @@ public class Bitacora {
 		return CTFecha;
 	}
 
+
 	public void setCTFecha(Date cTFecha) {
 		CTFecha = cTFecha;
 	}
-	
-	public String getCVRol() {
-		return CVRol;
-	}
-	
-	public void setCVRol(String cVRol) {
-		CVRol = cVRol;
-	}
-	
-	
+
 
 }
