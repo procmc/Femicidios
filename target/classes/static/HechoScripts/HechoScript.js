e04390403f3529894ba52edee1113136212eb778
Carlos Morales Castro <pro.cmc@gmail.com>
2023-06-03 09:26:41 -0600

    Actuctualizacion

--- target/classes/static/HechoScripts/HechoScript.js
+++ target/classes/static/HechoScripts/HechoScript.js
@@ -1,5 +1,5 @@
 function ocultarError() {
-    var errorMessage = document.getElementById("error-message");
+    let errorMessage = document.getElementById("error-message");
     if (errorMessage) {
         setTimeout(function () {
             errorMessage.style.display = "none";
