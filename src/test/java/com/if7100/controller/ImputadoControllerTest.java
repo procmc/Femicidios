1e768b962e29b9e7657f57219e841bf390aff5fe
hadjil <hadji.tiara@gmail.com>
2023-06-10 04:52:34 -0600

    Test

--- src/test/java/com/if7100/controller/ImputadoControllerTest.java
+++ src/test/java/com/if7100/controller/ImputadoControllerTest.java
@@ -15,7 +15,29 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class ImputadoControllerTest {
 
     @Autowired
-    private ImputadoRepository imputadoRepository;
+      private ImputadoRepository imputadoRepository;
+    private int ci_id=2;
+    private String cv_dni="699";
+    private String cv_nombre="Kijan Acuña Medrano";
+    private String cv_genero="Masculino";
+    private String cv_Orientacion_Sexual="Heterosexual";
+    private int ci_edad=24;
+private String CV_Lugar_Nacimiento="Purgatorio";
+private String cv_pais="Costa Rica";
+private String CV_Nacionalidad="Costarricense";     
+private String CV_Educacion="Primaria";  
+private String CV_ocupacion="Carnicero";
+private String CV_domicilio="Costa Rica"; 
+private String CV_Lugar_Residencia="Venezuela";
+private String CV_Condicion_Migratoria="Regular";
+private String  CV_Etnia="Negro";
+private String  CV_Situacion_Juridica="Fugitivo"; 
+private String  CV_Estado_Conyugal="NO"; 
+private String CV_Permiso_Portacion_Armas="NO";
+private String CV_Pertenencia_Fuerza_Seguridad="NO";
+private String  CV_Antecedentes="SI";
+private String CV_Suicidio="NO";
+private String CV_Generador="Organismo2";
 
     Imputado imputado = new Imputado("700260950", "MadaraKijan", "Masculino", "Hetero", "22", "BackTrack", "506");
 
@@ -50,6 +72,22 @@ public class ImputadoControllerTest {
     public void testCuatro() throws Exception{
         consultado = imputadoRepository.findByCVNombre("KijanMadara");
         imputadoRepository.delete(consultado);
+    }
+    
+    @Test
+    public void Test5() throws Exception {
+    	
+    	
+    	consultado = imputadoRepository.findByCVNombre(cv_nombre);
+    	consultado.setCVNombre(cv_nombre);
+    	imputadoRepository.save(consultado);
+    	consultado = imputadoRepository.findByCVNombre(cv_nombre);
+    	 assertEquals(consultado.getCVNombre(), cv_nombre);
+    	 assertEquals(consultado.getCVGenero(), cv_genero);
+    	 assertEquals(consultado.getCVOrientacionSexual(), cv_Orientacion_Sexual);
+    	 assertEquals(consultado.getCIEdad(), ci_edad);
+    	 assertEquals(consultado.getCVLugarNacimiento(),CV_Lugar_Nacimiento );
+    
     }
 
 }
