c7677ed5acc5bf76bc24861bd278a61248b2b4a1
hadjil <hadji.tiara@gmail.com>
2023-06-10 02:07:39 -0600

    Actualizacion de Victima
    Cambios

--- src/main/java/com/if7100/controller/VictimaController.java
+++ src/main/java/com/if7100/controller/VictimaController.java
@@ -6,6 +6,10 @@ package com.if7100.controller;
 import com.if7100.entity.Bitacora; 
 import com.if7100.entity.Usuario;
 import com.if7100.service.BitacoraService;
+import com.if7100.service.IdentidadGeneroService;
+import com.if7100.service.NivelEducativoService;
+import com.if7100.service.OrganismoService;
+import com.if7100.service.OrientacionSexualService;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -26,6 +30,10 @@ import com.if7100.service.PerfilService;
 
 import com.if7100.service.VictimaService;
 import com.if7100.entity.Hecho;
+import com.if7100.entity.IdentidadGenero;
+import com.if7100.entity.NivelEducativo;
+import com.if7100.entity.Organismo;
+import com.if7100.entity.OrientacionSexual;
 import com.if7100.entity.Perfil;
 import com.if7100.entity.Victima;
 import com.if7100.repository.UsuarioRepository;
@@ -34,6 +42,7 @@ import com.if7100.repository.UsuarioRepository;
 public class VictimaController {
 	
 	private VictimaService victimaService;
+	
 	//instancias para control de acceso
     private UsuarioRepository usuarioRepository;
     private Perfil perfil;
@@ -41,18 +50,36 @@ public class VictimaController {
   //instancias para control de bitacora
     private BitacoraService bitacoraService;
     private Usuario usuario;
+    //Instancias para el control de organismo
+    private Organismo organismo;
+    private OrganismoService organismoService;
+    //Instancias el control de Orientacion Sexual
+    private OrientacionSexualService orientacionSexualService;
+    private OrientacionSexual orientacionSexual;
+  //Instancias el control de Genero
+    private IdentidadGeneroService identidadGeneroService;
+    private IdentidadGenero identidadGenero;
+  //Instancias el control de Nivel educativo
+    private NivelEducativo nivelEducativo;
+    private NivelEducativoService nivelEducativoService;
 
-	
-	public VictimaController (BitacoraService bitacoraService,
-VictimaService victimaService, PerfilService perfilService, UsuarioRepository usuarioRepository)
-	{
+	//Constructor con todos las instancias
+	public VictimaController(VictimaService victimaService, UsuarioRepository usuarioRepository,
+			PerfilService perfilService, BitacoraService bitacoraService, OrganismoService organismoService,
+			OrientacionSexualService orientacionSexualService, IdentidadGeneroService identidadGeneroService,
+			NivelEducativoService nivelEducativoService) {
 		super();
-		this.victimaService=victimaService;
+		this.victimaService = victimaService;
+		this.usuarioRepository = usuarioRepository;
 		this.perfilService = perfilService;
-        this.usuarioRepository = usuarioRepository;
-        this.bitacoraService= bitacoraService;
-
+		this.bitacoraService = bitacoraService;
+		this.organismoService = organismoService;
+		this.orientacionSexualService = orientacionSexualService;
+		this.identidadGeneroService = identidadGeneroService;
+		this.nivelEducativoService = nivelEducativoService;
 	}
+
+
 	
 	private void validarPerfil() {
     	
@@ -85,7 +112,15 @@ VictimaService victimaService, PerfilService perfilService, UsuarioRepository us
 			if(!this.perfil.getCVRol().equals("Consulta")) {
 				
 				Victima victima = new Victima();
+				
+				model.addAttribute("orientacionSexual",orientacionSexualService.getAllOrientacionesSexuales());
+				model.addAttribute("identidadGenero",identidadGeneroService.getAllIdentidadGenero());
+				model.addAttribute("nivelEducativo",nivelEducativoService.getAllNivelEducativo());
+				model.addAttribute("listaOrganismo",organismoService.getAllOrganismos());
 				model.addAttribute("victima", victima);
+				
+				bitacoraService.saveBitacora(new Bitacora(this.usuario.getCI_Id(),
+						this.usuario.getCVNombre(),this.perfil.getCVRol(),"Crea en Victima"));
 				return "victimas/create_victima";
 			}else {
 				return "SinAcceso";
@@ -116,6 +151,8 @@ VictimaService victimaService, PerfilService perfilService, UsuarioRepository us
 				bitacoraService.saveBitacora(bitacora);
 				
 				victimaService.deleteVictimaById(Id);
+				bitacoraService.saveBitacora(new Bitacora(this.usuario.getCI_Id(),
+						this.usuario.getCVNombre(),this.perfil.getCVRol(),"Eliminó en Victima"));
 				return "redirect:/victima";
 			}else {
 				return "SinAcceso";
@@ -134,6 +171,10 @@ VictimaService victimaService, PerfilService perfilService, UsuarioRepository us
 			this.validarPerfil();
 			if(!this.perfil.getCVRol().equals("Consulta")) {
 				
+				model.addAttribute("orientacionSexual",orientacionSexualService.getAllOrientacionesSexuales());
+				model.addAttribute("identidadGenero",identidadGeneroService.getAllIdentidadGenero());
+				model.addAttribute("nivelEducativo",nivelEducativoService.getAllNivelEducativo());
+				model.addAttribute("listaOrganismo",organismoService.getAllOrganismos());
 				model.addAttribute("victima", victimaService.getVictimaById(id));
 				return "victimas/edit_victima";
 			}else {
@@ -174,8 +215,11 @@ VictimaService victimaService, PerfilService perfilService, UsuarioRepository us
 		existingVictima.setCIHijos(victima.getCIHijos());
 		existingVictima.setCVGenerador(victima.getCVGenerador());
 		
-		
 		victimaService.updateVictima(existingVictima);
+		
+		bitacoraService.saveBitacora(new Bitacora(this.usuario.getCI_Id(),
+				 this.usuario.getCVNombre(),this.perfil.getCVRol(),"Actualizó en Victima"));
+		 
 		return "redirect:/victima";
 		
 	}
