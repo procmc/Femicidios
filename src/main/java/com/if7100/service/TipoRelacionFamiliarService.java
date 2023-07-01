d3d493d537a039f4125c85102af071c0679325ff
Michael Arauz <Michaeltorrez928@gmail.com>
2023-06-30 18:27:32 -0600

    Revert "Revert "Tipo Relación Familiar""
    This reverts commit e057ea62173507bf375bf8e62c9e8ccccbdee8bf.

---
+++ src/main/java/com/if7100/service/TipoRelacionFamiliarService.java
@@ -0,0 +1,24 @@
+package com.if7100.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.if7100.entity.TipoRelacionFamiliar;
+
+
+public interface TipoRelacionFamiliarService {
+	List<TipoRelacionFamiliar> getAllTipoRelacionFamiliar();
+
+	Page<TipoRelacionFamiliar> getAllTipoRelacionFamiliar(Pageable pageable);
+
+	TipoRelacionFamiliar saveTipoRelacionFamiliar(TipoRelacionFamiliar identidadGenero);
+
+	TipoRelacionFamiliar getTipoRelacionFamiliarById(Integer Id);
+
+	TipoRelacionFamiliar updateTipoRelacionFamiliar(TipoRelacionFamiliar tiporelacion);
+	
+
+	void deleteTipoRelacionFamiliarById(Integer Id);
+}
