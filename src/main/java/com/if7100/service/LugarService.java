a46805fdf2c90cf598844c576cc848047cb00085
Ana Aguilar Vanegas <anayancyaguilarvanegas@gmail.com>
2023-07-04 19:10:09 -0600

    Update LugarService.java

--- src/main/java/com/if7100/service/LugarService.java
+++ src/main/java/com/if7100/service/LugarService.java
@@ -2,6 +2,8 @@ package com.if7100.service;
 
 import java.util.List;
 import com.if7100.entity.Lugar;
+import com.if7100.entity.TipoLugar;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -15,6 +17,8 @@ public interface LugarService {
     List<Lugar> getAllLugares(Integer CI_Hecho);
 
     List<Lugar> getAllLugar();
+    List<TipoLugar> getAllTipoLugars();
+
 
     Lugar saveLugar(Lugar lugar);
 
