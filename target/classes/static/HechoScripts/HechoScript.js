7d431fa177d2c3b0909c408c0244ed2e3e7f869e
Carlos Morales Castro <pro.cmc@gmail.com>
2024-10-24 22:33:19 -0600

    Delete target directory

--- target/classes/static/HechoScripts/HechoScript.js
+++
@@ -1,9 +0,0 @@
-function ocultarError() {
-    let errorMessage = document.getElementById("error-message");
-    if (errorMessage) {
-        setTimeout(function () {
-            errorMessage.style.display = "none";
-        }, 3000)
-        //errorMessage.style.display = "none";
-    }
-}
